2a2649aeddad72cac6629383e6c79d26841b1d43
Cody Bradbury <codybradbury05@gmail.com>
2024-06-01 15:33:41 +0000

    basicsss

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    if request.method == "GET":
+        return HttpResponse("You must have POSTed something")
+    else:
+         return HttpResponse(request.method)
